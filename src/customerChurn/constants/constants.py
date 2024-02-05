5633e561bde52798b3aef04974ccb46930059e62
jayarajmadanu <jayarajmadanu@gmail.com>
2024-02-05 12:46:31 +0530

    Project Setup

---
+++ src/customerChurn/constants/constants.py
@@ -0,0 +1,5 @@
+from pathlib import Path
+
+CONFIG_FILE_PATH= Path("config/customerChurnConfig.yaml")
+PARAMS_FILE_PATH= Path("params/customerChurnParams.yaml")
+SCHEMA_FILE_PATH= Path("schema/customerChurnSchema.yaml")
